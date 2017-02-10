23a112110127b36db1a79e619a9f4b7c1732d630
xiwen.yxw <xiwen.yxw@alibaba-inc.com>
2017-02-10 15:33:49 +0800

    modify file name

---
+++ src/sockpoll/ys_sockModule.h
@@ -0,0 +1,30 @@
+/**
+ * author : yue sivan
+ */
+#ifndef _SOCK_MODULE_H_
+#define _SOCK_MODULE_H_
+#include "sockdata.h"
+
+namespace ys {
+
+class sock_module {
+public:
+    sock_module();
+    ~sock_module();
+public:
+    void prepare(struct pollfd *psock, int& nsock);
+    void process(struct pollfd *psock);
+private:
+    listen_meta listen;
+    connect_meta* connList;
+    threadPool readPool;
+    threadPool writePool;
+private:
+    sock_module(const sock_module&);
+    sock_module& operator=(const sock_module&);
+};
+
+}
+
+#endif
+
