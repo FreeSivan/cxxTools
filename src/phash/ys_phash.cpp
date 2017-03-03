a8b1504b35a36cdac7b10e63fd0c4582c9316e80
xiwen.yxw <xiwen.yxw@alibaba-inc.com>
2017-03-03 16:06:34 +0800

    hash

--- src/phash/ys_phash.cpp
+++ src/phash/ys_phash.cpp
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <malloc.h>
+#include <assert.h>
 namespace ys {
 
 phash::phash() {
@@ -39,33 +40,24 @@ void phash::addphKey(unsigned int key, int length) {
 }
 
 void phash::addOver() {
-    printf ("----\n");
-    int key = lastKey + 1;
-    if (key < 0) {
-        return;
+    for (int i = lastKey+1; i < F_LENGTH; ++i) {
+        addphKey(i, 0);
     }
-    generate(key++);
-    if (key < 0) {
-        return;
+    for (int i = 0; i < T_LENGTH; ++i) {
+        printf ("%ld\n", third[i]);
     }
-    int i1 = key >> 21;
-    int i2 = key >> 6;
-    int i3 = key;
-    memset(first+i3, 0, sizeof(short)*(F_LENGTH-i3));
-    memset(second+i2, 0, sizeof(int)*(S_LENGTH-i2));
-    memset(third+i1, 0, sizeof(long)*(T_LENGTH-i1));
 }
 
 void phash::generate(unsigned int key) {
-    if (lastKey >> 6 != key >> 6) {
-        soffset += foffset;
-        second[key>>6] = soffset;
-        foffset = 0;
-    }
     if (lastKey >> 21 != key >> 21) {
         toffset += soffset;
         third[key>>21] = toffset;
         soffset = 0;
+    }
+    if (lastKey >> 6 != key >> 6) {
+        soffset += foffset;
+        second[key>>6] = soffset;
+        foffset = 0;
     }
     first[key] = foffset;
 }
@@ -81,9 +73,11 @@ int main() {
         hashtable.addphKey(i, 50);
     }
     hashtable.addOver();
+
     for (unsigned long i = 0; i < length; i+=4) {
         long offset = hashtable.getpHash(i);
-        printf ("%d offset = %d\n",i, offset);
+        printf ("%d %ld\n", i, offset);
+        assert (offset == (i/4)*50);
     }
     return 0;
 }
