c13059f2bd1df9c522b58a81b799422349d45c04
xiwen.yxw <xiwen.yxw@alibaba-inc.com>
2017-03-03 11:09:08 +0800

    hash

--- src/phash/ys_phash.cpp
+++ src/phash/ys_phash.cpp
@@ -6,6 +6,7 @@ phash::phash() {
     first = new unsigned short[F_LENGTH];
     second = new unsigned int[S_LENGTH];
     third = new unsigned long[T_LENGTH];
+    foffset = soffset = toffset = lastKey = 0;
 }
 
 phash::~phash() {
@@ -21,7 +22,39 @@ long phash::getpHash(unsigned int key) {
     return first[i1]+second[i2]+third[i3];
 }
 
+void phash::addphKey(unsigned int key, int length) {
+    if (lastKey+1 < key) {
+        unsigned int i = lastKey+1;
+        for (; i < key; ++i) {
+            generate(i);
+        }    
+    }
+    generate(key);
+    foffset += length;
+    lastKey = key;
+}
 
+void phash::addOver() {
+    int key = lastKey + 1;
+    int i1 = key >> 21;
+    int i2 = key >> 6;
+    int i3 = key;
+    memset(first+i1, 0, sizeof(int)*(F_LENGTH-i1));
+    memset(second+i2, 0, sizeof(int)*(S_LENGTH-i2));
+    memset(third+i3, 0, sizeof(int)*(F_LENGTH-i3));
+}
+
+void phash::generate(unsigned int key) {
+    if (lastKey >> 21 != key >> 21) {
+        third[key>>21] += soffset;
+        soffset = 0;
+    }
+    if (lastKey >> 6 != key >> 6) {
+        second[k>>6] += foffset;
+        foffset = 0;
+    }
+    first[key] = foffset;
+}
 
 }
 
--- src/phash/ys_phash.h
+++ src/phash/ys_phash.h
@@ -12,15 +12,21 @@ public:
     phash();
     ~phash();
 public:
+    void addOver();
     long getpHash(unsigned int key);
     void addphKey(unsigned int key, int length);
 private:
     unsigned short *first;
     unsigned int *second;
     unsigned long *third;
+    unsigned short foffset;
+    unsigned int soffset;
+    unsigned long toffset;
+    unsigned int lastKey;
 private:
     phash(const phash&);
     phash& operator=(const phash&);
+    void generate(unsigned int key, int length);
 };
 
 }
