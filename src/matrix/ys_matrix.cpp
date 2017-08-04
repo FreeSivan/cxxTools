ee66e82d430e25bb38f4a6012342983031a522f3
yuexiwen <yuexiwen@qiyi.com>
2017-08-04 20:52:27 +0800

    matrix

---
+++ src/matrix/ys_matrix.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <stdlib.h>
+
+#include "ys_matrix.h"
+
+double ran_uniform() {
+    return rand()/((double)RAND_MAX + 1);
+}
+
+
+int main(void) {
+    DMatrix<double> dmatrix(100, 1000);
+    for (int i = 0; i < dmatrix.getDimX(); ++i) {
+    for (int j = 0; j < dmatrix.getDimY(); ++j) {
+        dmatrix(i, j) = ran_uniform();
+    }
+    }
+    dmatrix.save("tmpfile");
+    return 0;
+}
--- src/matrix/ys_matrix.h
+++ src/matrix/ys_matrix.h
@@ -1,6 +1,4 @@
-#include <string>
-#include <iostream>
-
+#include <stdio.h>
 #include <../datapool/ys_datapool.h>
 
 namespace ys {     // namespace for yue sivan
@@ -11,65 +9,62 @@ template <typename T> class DMatrix {
 public:
     DMatrix();
     ~DMatrix();
-    DMatrix(const DMatrix<T>& matrix);
-    DMatrix(uint dim1, uint dim2);
+    DMatrix(uint x, uint y);
+    DMatrix(const DMatrix<T>& v);
 public:
     void init(T v);
     int getDimX() const;
     int getDimY() const;
     const dataPool<char*>& getColNames()const;
-    T get(uint x, unsigned y) const;
-    void set(uint x, unsigned y, T v);
+    T get(uint x, uint y) const;
+    void set(uint x, uint y, T v);
     void setSize(uint x, uint y);
     void assign(const DMatrix<T>& v);
 public:
-    void save(char* filename);
-    void saveBin(char* filename);
-    void load(char* filename);
-    void loadBin(char* filename);
+    bool save(char* filename);
+    bool load(char* filename);
 public:
-    DMatrix<T>& operator=(const DMatrix<T>& matrix);
+    DMatrix<T>& operator=(const DMatrix<T>& v);
     T& operator() (uint x, uint y);
-    T operator() (uint x, uint y);
+    T  operator() (uint x, uint y);
     T* operator() (uint x);
     T* operator[] (uint x); 
 private:
     T** value_;
-    uint dim1_;
-    uint dim2_;
-    dataPool<char*> col_names_;
+    uint dimX_;
+    uint dimY_;
 };
 
 template <typename T>
 inline DMatrix<T>::DMatrix() {
-    dim1_ = 0;
-    dim2_ = 0;
+    dimX_ = 0;
+    dimY_ = 0;
     value_ = 0;
 }
 
 template <typename T>
-inline DMatrix<T>::DMatrix(uint dim1, uint dim2) {
-    dim1_ = 0;
-    dim2_ = 0;
+inline DMatrix<T>::DMatrix(uint x, uint y) {
+    dimX_ = 0;
+    dimY_ = 0;
     value_ = 0;
-    setSize(dim1, dim2);
+    setSize(x, y);
 }
 
 template <typename T>
-inline DMatrix<T>::DMatrix(const DMatrix<T>& matrix) {
-    if (dim1_ != matrix.getDimX() || dim2_ != matrixDimY()) {
-        setSize(matrix.getDimX(), matrix.getDimY());
+inline DMatrix<T>::DMatrix(const DMatrix<T>& v) {
+    if (dimX_ != v.getDimX() || dimY_ != matrixDimY()) {
+        setSize(v.getDimX(), v.getDimY());
     }
-    for (int i = 0; i < dim1_; ++i) {
-        for (int j = 0; j < dim2_; ++j) {
-            value_[i][j] = matrix[i][j];
-        }
+    for (int i = 0; i < dimX_; ++i) {
+    for (int j = 0; j < dimY_; ++j) {
+        value_[i][j] = v[i][j];
+    }
     }
 }
 
 template <typename T>
 inline DMatrix<T>::~DMatrix() {
-    if (!value_) {
+    if (value_) {
         delete[] value_[0];
         delete[] value_;
     }
@@ -77,24 +72,24 @@ inline DMatrix<T>::~DMatrix() {
 
 template <typename T>
 inline void DMatrix<T>::init(T v) {
-    for (int i = 0; i < dim1_; ++i) {
-        for (int j = 0; j < dim2_; ++j) {
-            value_[i][j] = v;
-        }
+    for (int i = 0; i < dimX_; ++i) {
+    for (int j = 0; j < dimY_; ++j) {
+        value_[i][j] = v;
+    }
     }
 }
 
 template <typename T>
-inline T DMatrix<T>::get(uint x, unsigned y) const {
-    if (x > dim1_ || y > dim2_) {
+inline T DMatrix<T>::get(uint x, uint y) const {
+    if (x > dimX_ || y > dimY_) {
         throw x;
     }
     return value_[x][y];
 }
 
 template <typename T>
-inline void DMatrix<T>::set(uint x, unsigned y, T value) {
-    if (x > dim1_ || y > dim2_) {
+inline void DMatrix<T>::set(uint x, uint y, T value) {
+    if (x > dimX_ || y > dimY_) {
         throw x;
     }
     value_[x][y] = T;
@@ -105,58 +100,97 @@ inline void DMatrix<T>::setSize(uint x, uint y) {
     if ((dim1 == x) && (dim2 == y)) {
         return;
     }
+    dimX_ = x;
+    dimY_ = y;
     if (!value) {
         delete[] value_[0];
         delete[] value;
     }
-    dim1_ = x;
-    dim2_ = y;
     value = new T*[dim1];
     value[0] = new T[dim1 * dim2];
-    for (uint i = 0; i < dim1_; ++i) {
-        value_[i] = value_[0] + i * dim2_;
+    for (uint i = 0; i < dimX_; ++i) {
+        value_[i] = value_[0] + i * dimY_;
     }
-    col_names.resize(dim2_);
 }
 
 template <typename T>
 inline void DMatrix<T>::assign(const DMatrix<T>& v) {
+    if (v.getDimX() != dimX_ || v.getDimY() != dimY_) {
+        setSize(v.getDimX(), v.getDimY());
+    }
+    for (int i = 0; i < dimX_; ++i) {
+    for (int j = 0; j < dimY_; ++j) {
+        value_[i][j] = v[i][j];
+    }
+    }
 }
 
 template <typename T>
-inline void DMatrix<T>::save(char* filename) {
+inline DMatrix<T>& DMatrix<T>::operator=(const DMatrix<T>& v) {
+    assign(v);
+    return *this;
 }
 
 template <typename T>
-inline void DMatrix<T>::saveBin(char* filename) {
+inline T& DMatrix<T>::operator() (uint x, uint y) {
+    return value_[x][y]
 }
 
 template <typename T>
-inline void DMatrix<T>::load(char* filename) {
+inline T  DMatrix<T>::operator() (uint x, uint y) {
+    return value_[x][y]
 }
 
 template <typename T>
-inline void DMatrix<T>::loadBin(char* filename) {
+inline T* DMatrix<T>::operator() (uint x) {
+    return value[x];
 }
 
 template <typename T>
-DMatrix<T>& DMatrix<T>::operator=(const DMatrix<T>& matrix) {
+inline T* DMatrix<T>::operator[] (uint x) {
+    return value[x];
 }
 
 template <typename T>
-T& DMatrix<T>::operator() (uint x, uint y) {
+inline bool DMatrix<T>::save(char* filename) {
+    FILE *fp = fopen(filename, "wb");
+    if (!fp) {
+        goto Error;
+    }
+    fwrite(&dimX_, sizeof(uint), 1, fp);
+    fwrite(&dimY_, sizeof(uint), 1, fp);
+    for (int i = 0; i < dimX_; ++i) {
+        fwrite(value_[i], sizeof(T), dimY_, fp);
+    }
+    fclose(fp);
+
+Finish:
+    return 1;
+Error:
+    return 0;
 }
 
 template <typename T>
-T DMatrix<T>::operator() (uint x, uint y) {
-}
-
-template <typename T>
-T* DMatrix<T>::operator() (uint x) {
-}
-
-template <typename T>
-T* DMatrix<T>::operator[] (uint x) {
+inline bool DMatrix<T>::load(char* filename) {
+    FILE *fp = fopen(filename, "rb");
+    if (!fp) {
+        goto Error:
+    }
+    int dimx, dimy;
+    fread(&dimx, sizeof(uint), 1, fp);
+    fread(&dimy, sizeof(uint), 1, fp);
+    if (dimx != dimX_ || dimy != dimY_) {
+        setSize(dimx, dimy);
+    }
+    for (int i = 0; i < dimX_; ++i) {
+        fread(value_[i], sizeof(T), dimY_, fp);
+    }
+    fclose(fp);
+    
+Finish:
+    return 1;
+Error:
+    return 0;
 }
 
 };
