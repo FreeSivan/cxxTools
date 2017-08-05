27640f2a0e844849e28f55590eee11ddc31ed5cf
yuexiwen <yuexiwen@qiyi.com>
2017-08-05 11:36:49 +0800

    modify

--- src/matrix/ys_matrixTool.cpp
+++ src/matrix/ys_matrixTool.cpp
@@ -26,11 +26,15 @@ int main(void) {
     matrix1[0][0] = 8;
         printf ("1\n");
     matrix1[0][1] = 9;
-    matrix1[1][0] = 1;
-    matrix1[1][1] = 6;
-    matrix1[2][0] = 3;
-    matrix1[2][1] = 8;
     printf ("2\n");
+    matrix1[1][0] = 1;
+    printf ("3\n");
+    matrix1[1][1] = 6;
+    printf ("4\n");
+    matrix1[2][0] = 3;
+    printf ("5\n");
+    matrix1[2][1] = 8;
+    printf ("6\n");
 
     for (int i = 0; i < matrix2.getDimX(); ++i) {
     for (int j = 0; j < matrix2.getDimY(); ++j) {
