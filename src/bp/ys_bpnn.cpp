3800c7b85e0d3ba2af69073119d629b7012527e0
yuexiwen <yuexiwen@qiyi.com>
2017-08-10 21:19:24 +0800

    modify bp

--- src/bp/ys_bpnn.cpp
+++ src/bp/ys_bpnn.cpp
@@ -2,60 +2,209 @@
 
 namespace ys {
 
+/*
+ *Summary: 神经元base类析构函数
+ *      
+ *      申请weight_的内存，不申请input_
+ *
+ *      input_在layer层申请
+ *
+ *Parameters:
+ *
+ *      无
+ *
+ *Return : 无
+ *
+ */
+
 BPNode::BPNode(int wNum) {
     wNum_ = wNum;
-    w_ = new double[wNum_];
-    b_ = 0;
-    s_ = 0;
-    o_ = 0;
+    weight_ = new double[wNum_];
+    input_ = NULL;
+    output_ = bias_ = sum_ = 0;
 }
 
+/*
+ *Summary: 神经元base类析构函数
+ *      
+ *      负责释放weight_数组，不释放input_
+ *
+ *      input_在layer层维护，神经元只读访问
+ *
+ *Parameters:
+ *
+ *      无
+ *
+ *Return : 无
+ *
+ */
 BPNode::~BPNode() {
-    delete[] w_;
+    delete[] weight_;
 }
 
-BPLayer::BPLayer(int cNum, int pNum, int index) {
-    curNum_ = cNum;
-   cur_ = new BPNode*[curNum_];
-    for (int i = 0; i < curNum_; ++i) {
-        cur_[i] = new BPNode(pNum);
+/*
+ *Summary: 加和函数
+ *      
+ *      计算神经元输入关于权重的加权和
+ *
+ *Parameters:
+ *
+ *      无
+ *
+ *Return : 输入*权重+偏置
+ */
+double BPNode::additiveFunc() {
+    if (!weight_ || !input_) {
+        return -1;
     }
-    pre_ = 0;
-    post_ = 0;
+    sum_ = 0;
+    for (int i = 0; i < wNum_; ++i) {
+        sum_ += weight_[i] * input_[i];
+    }
+    sum_ += bias_;
+    return sum_;
+}
+
+/*
+ *Summary: 激活函数
+ *      
+ *      将神经元的输出经过激活函数
+ *
+ *Parameters:
+ *
+ *      无
+ *
+ *Return : 经过激活函数计算神经元最后输出值
+ */
+double BPNode::activationFunc() {
+    
+}
+
+/*
+ *Summary: 加和函数关于输入的偏导函数
+ *      
+ *      计算第pos个输入关于加和函数的偏导值
+ *
+ *Parameters:
+ *
+ *      int pos : 输入的编号
+ *
+ *Return : 第pos个输入关于加和函数的偏导值
+ *
+ */
+double BPNode::derivativeI(int pos) {
+    if (!weight_) {
+        return -1;
+    }
+    return weight_[pos];
+}
+
+/*
+ *Summary: 加和函数关于权重的偏导函数
+ *      
+ *      计算第pos个权重关于加和函数的偏导值
+ *
+ *Parameters:
+ *
+ *      int pos : 权重的编号
+ *
+ *Return : 第pos个权重关于加和函数的偏导值
+ *
+ */
+double BPNode::derivativeW(int pos) {
+    if (!input_) {
+        return -1;
+    }
+    return input_[pos];
+}
+
+/*
+ *Summary: 获取神经元的第index个权重
+ *
+ *Parameters:
+ *
+ *      int index : 带获取的权重的编号
+ *
+ *Return : 神经元第index个权重值
+ *
+ */
+ double BPNode::getWeight(int index) {
+    if (index >= weight_ || !weight_) {
+        return -1;
+    }
+    return weight_[index];
+}
+
+ /*
+  *Summary: 获取神经元的第index个权重
+  *
+  *Parameters:
+  *
+  *      int index : 带获取的权重的编号
+  *
+  *Return : 神经元第index个权重值
+  *
+  */
+BPLayer::BPLayer(int iNum, int oNum, int index) {
+    inputNum_ = iNum;
+    outputNum_ = oNum;
     layerIndex_ = index;
+    lLink_ = rLink_ = NULL;
+    node_ = new BPNode*[oNum];
+    for (int i = 0; i < oNum; ++i) {
+        BPNode[i] = NULL;
+    }
 }
 
 BPLayer::~BPLayer() {
-    for (int i = 0; i < curNum_; ++i) {
-        delete cur_[i];
+    // TODO
+}
+
+double BPLayer::getOutput(int index) const {
+    if (index >= outputNum_ || !node_) {
+        return -1;
     }
-    delete[] cur_;
-}
-
-void BPLayer::setPre(BPNode** pre, int pNum) {
-    pre_ = pre;
-    preNum_ = pNum;
-}
-
-void BPLayer::setPost(BPNode** post, int pNum) {
-    post_ = post;
-    postNum_ = pNum;
-}
-
-void BPLayer::forward() {
-    for (int i = 0; i < curNum_; ++i) {
-        double sum = 0;
-        for (int j = 0; j < preNum_; ++j) {
-            sum += cur_[i]->w_[j]*pre_[j]->o_;
-        }
-        cur_[i]->s_ = sum;
-        double out = activeFunc(sum);
-        cur_[i]->o_ = out;
+    if (node_[index]) {
+        return -1;
     }
+    return node_[index]->output_;
 }
 
-double BPLayer::activeFunc(double sum) {
-    return sum; 
+double BPLayer::getAdditive(int index) const {
+    if (index >= outputNum_ || !node_) {
+        return -1;
+    }
+    if (node_[index]) {
+        return -1;
+    }
+    return node_[index]->sum_;
+}
+
+double BPLayer::getWeight(int oIndex, int wIndex) {
+    if (oIndex >= outputNum_ || !node_) {
+        return -1;
+    }
+    if (!node_[oIndex]) {
+        return -1;
+    }
+    return node_[oIndex]->getWeight(wIndex);
+}
+
+double BPLayer::getLayerIndex() const {
+    return layerIndex_;
+}
+
+bool BPLayer::forward() {
+    if (!input_ || !node_) {
+        return false;
+    }
+    for (int i = 0; i < inputNum_; ++i) {
+        input_[i] = lLink_->getOutput(i);
+    }
+    for (int i = 0; i < outputNum_; ++i) {        
+        node_[i]->additiveFunc()
+        node_[i]->activationFunc();
+    }
 }
 
 }
--- src/bp/ys_bpnn.h
+++ src/bp/ys_bpnn.h
@@ -11,12 +11,21 @@ public:
     BPNode(int wNum);
     ~BPNode();
     friend class BPLayer;
-private:
-    int wNum_;      // weight num;
-    double *w_;     // weight array;
-    double b_;      // bias
-    double s_;      // sum
-    double o_;      // output
+public:
+    double additiveFunc();
+    double derivativeAdditiveW(int pos);
+    double derivativeAdditiveI(int pos);
+    double getWeight(int index) const;
+public:
+    virtual double activationFunc();
+    virtual double derivativeActiation();
+protected:
+    int wNum_;          // 权值参数维度
+    double *weight_;    // 指向权值数组的指针
+    double *input_;     // 指向输入数组的指针
+    double output_;     // 节点最后输出
+    double bias_;       // 偏置项
+    double sum_;        // 输入的加权和
 private:
     BPNode(const BPNode&);
     void operator=(const BPNode&);
@@ -24,23 +33,25 @@ private:
 
 class BPLayer {
 public:
-    BPLayer(int curNum, int preNum, int index);
-    ~BPLayer();
+    BPLayer(int iNum, int oNum, int index);
+    virtual ~BPLayer();
     friend class BPNet;
 public:
-    void setPre(BPNode** pre, int num);
-    void setPost(BPNode** post, int num);
+    virtual void forward();
+    
 public:
-    void forward();
-    virtual double activeFunc(double sum);
+    double getOutput(int index) const;
+    double getAdditive(int index) const;
+    double getWeight(int oIndex, int wIndex) const;
+    double getLayerIndex() const;
 private:
-    int curNum_;
-    int preNum_;
-    int postNum_;
-    BPNode **cur_;
-    BPNode **pre_;
-    BPNode **post_;
-    int layerIndex_;
+    int inputNum_;      // 输入维度==权重维度
+    int outputNum_;     // 输出维度==本层节点个数
+    double *input_;     // 输入数组，存放本层输入
+    BPNode **node_;     // 本层节点数组
+    BPLayer *lLink_;    // 指向前一层的指针
+    BPLayer *rLink_;    // 指向后一层的指针
+    int layerIndex_;    // 当前层次的编号
 private:
     BPLayer(const BPLayer&);
     void operator=(const BPLayer&);
@@ -48,13 +59,21 @@ private:
 
 class BPNet {
 public:
-    BPNet();
-    ~BPNet();
+    BPNet(int layNum, int iNum, int oNum);
+    virtual ~BPNet();
+public:
+    bool addLayer();
+public:
+    virtual double lossFunc();
+    virtual double errorFunc();
+    virtual double regularFunc();
 private:
-    int layNum_;
-    BPLayer *lay_;    
-    double lRate_;      // learning rate
-    double rRate_;      // regularization rate
+    int layNum_;        // 圣经网络层数
+    int inputNum_;      // 神经网络输入维度
+    int outputNum_;     // 神经网络输出维度
+    BPLayer *lay_;      // 节点层的数组
+    double lRate_;      // 学习率
+    double rRate_;      // 正则率
 };
 
 }
